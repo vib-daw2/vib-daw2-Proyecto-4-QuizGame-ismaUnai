481518e02bf183cced2d1b7f1dfd67e6ff9131e1
IsmaelPolanco <ivelozpolanco@gmail.com>
2024-04-28 23:03:41 +0200

    falta que recorrer preguntas y podio, ponerlo mas bonito y cosas extras

---
+++ src/components/JoinScreen.js
@@ -0,0 +1,11 @@
+function JoinScreen({ start }) {
+  return (
+  <div className="join-screen">
+    <h2>Join Quiz</h2>
+    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas </p>
+    <button onClick={start}>Start</button>
+  </div>
+  );
+}
+
+export default JoinScreen;
---
+++ src/components/QuizScreen.js
@@ -0,0 +1,52 @@
+import {useState} from "react";
+
+import QuestionList from "../data/questions.json";
+import QuizResult from "./QuizResult.js";
+import Question from "./Question.js";
+function QuizScreen({retry}) {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length))
+    const isQuestionEnd = currentQuestionIndex === QuestionList.length;
+
+    function calculateResult() {
+        let correct =0;
+        QuestionList.forEach((question,index) => {
+            if (question.correctOptionIndex == markedAnswers[index]) {
+                correct++;
+            }
+        });
+        return {
+            total:QuestionList.length,
+            correct:correct,
+            percentage:Math.trunc((correct / QuestionList.length) * 100)
+        };
+    }
+    return (
+        <div className="quiz-screen">
+{
+    isQuestionEnd ? (
+        <QuizResult 
+            result={calculateResult()}
+            retry={retry}
+        />
+    ) : (
+        <Question
+            question={QuestionList[currentQuestionIndex]}
+            totalQuestions={QuestionList.length}
+            currentQuestion={currentQuestionIndex+1}
+            setAnswer={(index)=>{
+                setMarkedAnswers((arr)=>{
+                    let newArr = [...arr];
+                    newArr[currentQuestionIndex-1] = index;
+                    return newArr;
+                });
+                setCurrentQuestionIndex(currentQuestionIndex+1);
+            }}
+            />
+        )
+    }
+        </div>
+    );
+}
+
+export default QuizScreen;
